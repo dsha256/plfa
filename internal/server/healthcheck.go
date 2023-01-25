72724ce58e2b693f2ec5c25f7411cee2305ab3ec
dsha256 <shainidzedavid@gmail.com>
2023-01-25 23:56:14 +0400

    docs: add swagger UI

--- internal/server/healthcheck.go
+++ internal/server/healthcheck.go
@@ -4,11 +4,29 @@ import (
 	"net/http"
 )
 
+type systemInfo struct {
+	Version string `json:"version"`
+}
+
+type envelopeHealthCheck struct {
+	Status     string     `json:"status"`
+	SystemInfo systemInfo `json:"system_info"`
+}
+
+// healthcheckHandler shows if the service is up or not
+//
+// @Summary Check if the service is up
+// @Schemes
+// @Description Check if the service is up
+// @Accept json
+// @Produce json
+// @Success 200 {object} envelopeHealthCheck
+// @Router /healthcheck [get]
 func (s *Server) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	env := envelope{
-		"status": "available",
-		"system_info": map[string]string{
-			"version": version,
+	env := envelopeHealthCheck{
+		Status: "available",
+		SystemInfo: systemInfo{
+			Version: version,
 		},
 	}
 	err := s.writeJSON(w, http.StatusOK, env, nil)
